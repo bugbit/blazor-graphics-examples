61dc7ad9fce021d7d28a9f300b0a9907d6ee1263
bugbit <ohernandezba@gmail.com>
2022-11-05 09:29:57 +0100

    Core

---
+++ BlazorGraphicsExamples/Core/BAApp.cs
@@ -0,0 +1,38 @@
+#region License
+
+/*
+MIT License
+
+Copyright (c) 2022 bugbit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#endregion
+
+namespace BlazorGraphicsExamples.Core;
+
+public class BAApp
+{
+    public string Name { get; set; } = "App1";
+    public int Width { get; set; } = 800;
+    public int Height { get; set; } = 600;
+    public string UrlRoot { get; set; } = "";
+    public Assets.Assets Assets { get; } = new Assets.Assets();
+}
--- BlazorGraphicsExamples/Program.cs
+++ BlazorGraphicsExamples/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using blazor_graphics_examples;
+using BlazorGraphicsExamples;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
